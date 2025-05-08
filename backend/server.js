fc895a5206d580d45e5c1ce91072739f8ba34759
SohitBennett <E22CSEU1409@bennett.edu.in>
2025-05-09 00:34:01 +0530

    Updated the cors code lines second time

--- backend/server.js
+++ backend/server.js
@@ -16,10 +16,16 @@ const port = process.env.PORT || 5000;
 //     origin: process.env.FRONTEND_URL || "*", // safer to whitelist your frontend URL
 //     credentials: true
 // }));
+// app.use(cors({
+//     origin: process.env.FRONTEND_URL, 
+//     methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
+//     allowedHeaders: ['Content-Type', 'Authorization'],
+// }));
 app.use(cors({
-    origin: process.env.FRONTEND_URL, 
+    origin: ['https://nextjs-todo-list-vert-ten.vercel.app', process.env.FRONTEND_URL || '*'],
     methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
     allowedHeaders: ['Content-Type', 'Authorization'],
+    credentials: true
 }));
 app.use(express.json());
 
