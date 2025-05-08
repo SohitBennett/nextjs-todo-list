a371c1090c5df56204215b8deb5e8acfa6b7815d
SohitBennett <E22CSEU1409@bennett.edu.in>
2025-05-08 16:54:22 +0530

    Switched from local MongoDB to MongoDB Atlas

--- backend/server.js
+++ backend/server.js
@@ -8,9 +8,14 @@ import authRoutes from "./routes/authRoutes.js";
 
 dotenv.config();
 const app = express();
+const port = process.env.PORT || 5000;
 
 // Middlewares
-app.use(cors());
+// app.use(cors());
+app.use(cors({
+    origin: process.env.FRONTEND_URL || "*", // safer to whitelist your frontend URL
+    credentials: true
+}));
 app.use(express.json());
 
 // Routes 
@@ -23,7 +28,7 @@ mongoose.connect(process.env.MONGO_URI, {
     useUnifiedTopology: true,
 })
 .then(() => {
-    app.listen(5000, ()=> console.log("Server is running on port 5000"));
+    app.listen(port, ()=> console.log("Server is running on port 5000"));
 })
 
 .catch(err => console.error(err));
